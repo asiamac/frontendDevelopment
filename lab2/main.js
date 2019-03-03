344cccf241575b95c1f9bc6732322f650c57eb5a
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-03-03 11:05:49 +0100

    lab2: create database

---
+++ lab2/main.js
@@ -0,0 +1,77 @@
+var cloth1 = {
+    type: 'jeans',
+    model: 'slim',
+    size: 28,
+    forMen: false,
+    price: 270,
+    colors: ['dark indigo', 'light wash blue'],
+    outlet: {
+        outlet: false,
+        year: 2019
+    }
+};
+
+var cloth2 = {
+    type: 'jeans',
+    model: 'skinny',
+    size: 29,
+    forMen: false,
+    price: 290,
+    colors: ['dark indigo', 'white'],
+    outlet: {
+        outlet: true,
+        year: 2018
+    }
+};
+
+var cloth3 = {
+    type: 'jeans',
+    model: 'bootcut',
+    size: 30,
+    forMen: false,
+    price: 250,
+    colors: ['light wash blue'],
+    outlet: {
+        outlet: false,
+        year: 2019
+    }
+};
+
+var cloth4 = {
+    type: 'jeans',
+    model: 'relaxed',
+    size: 31,
+    forMen: true,
+    price: 300,
+    colors: ['navy blue', 'grey'],
+    outlet: {
+        outlet: true,
+        year: 2018
+    }
+};
+
+var cloth5 = {
+    type: 'jeans',
+    model: 'loose',
+    size: 32,
+    forMen: true,
+    price: 280,
+    colors: ['grey', 'white'],
+    outlet: {
+        outlet: false,
+        year: 2019
+    }
+};
+
+var cloth6 = {
+    type: 'jeans',
+    model: 'straight',
+    size: 33,
+    forMen: true,
+    price: 260,
+    colors: ['navy blue'],
+    outlet: {
+        outlet: false,
+        year: 2019
+    }
+};

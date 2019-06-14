259cda38e24cf24caacd7a874a384bdbcceb7867
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-06-14 23:54:07 +0200

    lab10: fixes

---
+++ lab10/lab10/src/Parent.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Dollar from './Dollar';
+import PLN from './PLN';
+
+class Parent extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dollar: 1,
+      pln: 2
+    }
+  }
+
+  rate = 3.83439994
+
+  handleDollarChange = (value) => {
+    this.setState({ dollar: value, pln: value * this.rate })
+  }
+
+  handlePLNChange = (value) => {
+    this.setState({ dollar: value / this.rate, pln: value })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Dollar dollars={this.state.dollar} onDollarChange={this.handleDollarChange}></Dollar>
+        <PLN plns={this.state.pln} onPLNChange={this.handlePLNChange}></PLN>
+      </div>
+    )
+  }
+}
+
+export default Parent

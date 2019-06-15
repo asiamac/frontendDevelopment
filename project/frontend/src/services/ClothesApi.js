ae7a589bfcd7084e4c1a867dad300dfd8c2f4981
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-06-15 20:10:14 +0200

    project: add checks for model correctness

--- project/frontend/src/services/ClothesApi.js
+++ project/frontend/src/services/ClothesApi.js
@@ -6,22 +6,43 @@ export const getClothesPrices = async () => {
   return await axios.get(`${url}/api/clothes/prices`);
 }
 
-export const addClothesPrices = async (prices) => {
-  return await axios.post(`${url}/api/clothes/prices`, prices);
+const validate = (object) => {
+  if (!object.name) {
+    throw Error('name field is required for every POST api call!');
+  }
+}
+
+export const addClothPrice = async (cloth) => {
+  validate(cloth);
+
+  if (!cloth.price)
+    throw Error('price field is required for addClothesPrices!');
+
+  return await axios.post(`${url}/api/clothes/prices`, cloth);
 }
 
 export const getClothesModels = async () => {
   return await axios.get(`${url}/api/clothes/models`);
 }
 
-export const addClothesModels = async (prices) => {
-  return await axios.post(`${url}/api/clothes/models`, prices);
+export const addClothModel = async (cloth) => {
+  validate(cloth);
+
+  if (!cloth.model)
+    throw Error('model field is required for addClothesModels!');
+
+  return await axios.post(`${url}/api/clothes/models`, cloth);
 }
 
 export const getClothesOutlets = async () => {
   return await axios.get(`${url}/api/clothes/prices`);
 }
 
-export const addClothesOutlets = async (prices) => {
-  return await axios.post(`${url}/api/clothes/prices`, prices);
+export const addClothOutlet = async (cloth) => {
+  validate(cloth);
+
+  if (!cloth.outlet)
+    throw Error('outlet field is required for addClothOutlet!');
+
+  return await axios.post(`${url}/api/clothes/prices`, cloth);
 }

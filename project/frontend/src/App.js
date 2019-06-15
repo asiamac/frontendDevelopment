82c1d51a3e0a9c7ede240fc704f401441791bb67
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-06-15 17:18:20 +0200

    project: move component to separate folder, add models and services

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Parent from './Parent'
+import Parent from './components/Parent'
 
 function App() {
   return (

ead60fc11ef9cd3b53222bcd1b6979d3c030ab3a
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-06-15 18:28:18 +0200

    project: add routing

---
+++ project/frontend/src/components/About.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <div>Store clothes database</div>
+        <a href="https://github.com/asiamac/frontendDevelopment">Repository</a>
+      </div>
+    );
+  }
+}
+
+export default About;
--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -1,12 +1,30 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Link, Route, Router, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import About from './components/About';
+import Converter from './components/Converter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <div>
+      <Link to={'/'}>Main page</Link><br />
+      {/* <Link to={'/addCloth'}>Add cloth</Link><br/>
+            <Link to={'/updateCloth'}>Update cloth</Link><br/> */}
+      <Link to={'/converter'}>Convert</Link><br />
+      <Link to={'/about'}>About page</Link>
+      <hr />
+      <Switch>
+        <Route exact path='/' component={App} />
+        {/* <Route path='/addCloth' component={AddClothForm} />
+                <Route path='/updateCloth' component={UpddateClothForm} /> */}
+        <Route path='/converter' component={Converter} />
+        <Route path='/about' component={About} />
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById('root'));

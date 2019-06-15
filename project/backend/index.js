208061449cab6e7442509f2fa2c9c6988345b893
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-06-16 01:39:09 +0200

    project: add sample data

--- project/backend/index.js
+++ project/backend/index.js
@@ -15,7 +15,44 @@ app.use((req, res, next) => {
   next();
 });
 
-let clothes = [];
+const clothes = [
+  {
+    forMen: false,
+    id: 'acd958e2-7d74-4a02-9fcd-e460270289d9',
+    model: 'skinny',
+    outlet: false,
+    price: 199,
+    size: 5,
+    type: 'jeans'
+  },
+  {
+    forMen: false,
+    id: 'ba3e2df3-7f02-4d35-9df5-e3ee0c5f8e69',
+    model: 'cotton',
+    outlet: true,
+    price: 59,
+    size: 9,
+    type: 'shirt'
+  },
+  {
+    forMen: true,
+    id: '77c51207-ad47-46c2-8901-dd0c42d87d06',
+    model: 'summer',
+    outlet: false,
+    price: 29,
+    size: 6,
+    type: 'hat'
+  },
+  {
+    forMen: false,
+    id: '01cc9a29-5827-4a0f-a8de-824b3c47fd8b',
+    model: 'winter',
+    outlet: true,
+    price: 399,
+    size: 7,
+    type: 'shoes'
+  }
+];
 
 app.get('/api/', (req, res) => {
   res.send('Clothes API');

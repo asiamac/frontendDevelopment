7eed50186543250d84d123100cc5dcb03d786e5a
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-06-15 18:10:27 +0200

    project: add status codes for api responses

--- project/backend/index.js
+++ project/backend/index.js
@@ -13,33 +13,41 @@ const outlets = [];
 app.get('/api/', (req, res) => res.send('Clothes API'));
 
 app.get('/api/clothes/prices', (req, res) => {
+  res.statusCode = 200;
   res.send(prices);
 });
 
 app.post('/api/clothes/prices', (req, res) => {
   const price = req.body.price;
   prices.push(price);
+
+  res.statusCode = 201;
   res.send({ message: 'Price added to database' });
 });
 
 app.get('/api/clothes/models', (req, res) => {
+  res.statusCode = 200;
   res.send(models);
 });
 
 app.post('/api/clothes/models', (req, res) => {
   const model = req.body.model;
   models.push(model);
+
+  res.statusCode = 201;
   res.send({ message: 'Model added to database' });
 });
 
 app.get('/api/clothes/outlets', (req, res) => {
+  res.statusCode = 200;
   res.send(outlets);
 });
 
 app.post('/api/clothes/outlets', (req, res) => {
   const outlet = req.body.outlet;
-
   outlets.push(outlet);
+
+  res.statusCode = 201;
   res.send({ message: 'Outlet information added to database' });
 });
 

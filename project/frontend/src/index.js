bf524bf92fa334d0a0e0ce9aadd0b8415fcb559f
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2019-06-15 18:59:21 +0200

    project: fix problem with routing

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Link, Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import './index.css';
 import App from './App';
@@ -15,14 +15,14 @@ ReactDOM.render(
       <Link to={'/'}>Main page</Link><br />
       {/* <Link to={'/addCloth'}>Add cloth</Link><br/>
             <Link to={'/updateCloth'}>Update cloth</Link><br/> */}
-      <Link to={'/converter'}>Convert</Link><br />
+      {/* <Link to={'/converter'}>Convert</Link><br /> */}
       <Link to={'/about'}>About page</Link>
       <hr />
       <Switch>
         <Route exact path='/' component={App} />
         {/* <Route path='/addCloth' component={AddClothForm} />
                 <Route path='/updateCloth' component={UpddateClothForm} /> */}
-        <Route path='/converter' component={Converter} />
+        {/* <Route path='/converter' component={Converter} /> */}
         <Route path='/about' component={About} />
       </Switch>
     </div>
